aa8335cd9a777abd4a5f275086e6d8e973caa52f
isabela-cortorreal <78940846+isabela-cortorreal@users.noreply.github.com>
2021-03-01 19:41:10 -0400

    Add files via upload

---
+++ ejercicio array 5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_5_array
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string[] nombre = new string[100];
+            int cont = 0;
+            do
+            {
+                Console.Write("Ingrese el nombre numero {0}: ", cont);
+                cont += 1;
+                nombre[cont] = Convert.ToString(Console.ReadLine());
+            } while (nombre[cont] != "");
+
+            Console.WriteLine("\n");
+            for (cont = 0; cont < 101; cont++)
+            {
+                Console.WriteLine("El nombre numero {0} ", cont + " es: " + nombre[cont]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
